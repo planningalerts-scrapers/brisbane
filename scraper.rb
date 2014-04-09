f07fb8bab864044c2c695ea385536c40976bd84b
Matthew Landauer <matthew@openaustralia.org>
2014-04-09 11:24:55 +1000

    Get it working for brisbane

--- scraper.rb
+++ scraper.rb
@@ -3,19 +3,20 @@ require 'mechanize'
 
 # Scraping from Masterview 2.0
 
-def scrape_page(page, comment_url)
-  page.at("table.rgMasterTable").search("tr.rgRow,tr.rgAltRow").each do |tr|
-    tds = tr.search('td').map{|t| t.inner_html.gsub("\r\n", "").strip}
-    day, month, year = tds[2].split("/").map{|s| s.to_i}
+def scrape_page(page)
+  page.at("table#_ctl2_pnl table").search("tr")[2..-1].each do |tr|
+    tds = tr.search('td').map{|t| t.inner_text.gsub("\r\n", "").strip}
+    p tds
+    day, month, year = tds[3].split("/").map{|s| s.to_i}
     record = {
       "info_url" => (page.uri + tr.search('td').at('a')["href"]).to_s,
-      "council_reference" => tds[1],
+      "council_reference" => tds[1].split(" - ")[0].squeeze(" ").strip,
       "date_received" => Date.new(year, month, day).to_s,
-      "description" => tds[3].gsub("&amp;", "&").split("<br>")[1].squeeze(" ").strip,
-      "address" => tds[3].gsub("&amp;", "&").split("<br>")[0].gsub("\r", " ").gsub("<strong>","").gsub("</strong>","").squeeze(" ").strip,
-      "date_scraped" => Date.today.to_s,
-      "comment_url" => comment_url
+      "description" => tds[1].split(" - ")[1..-1].join(" - ").squeeze(" ").strip,
+      "address" => tds[2].squeeze(" ").strip,
+      "date_scraped" => Date.today.to_s
     }
+    record["comment_url"] = "https://sde.brisbane.qld.gov.au/services/startDASubmission.do?direct=true&daNumber=" + CGI.escape(record["council_reference"]) + "&sdeprop=" + CGI.escape(record["address"])
     #p record
     if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
       ScraperWiki.save_sqlite(['council_reference'], record)
@@ -25,26 +26,7 @@ def scrape_page(page, comment_url)
   end
 end
 
-# Implement a click on a link that understands stupid asp.net doPostBack
-def click(page, doc)
-  js = doc["href"] || doc["onclick"]
-  if js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
-    event_target = $1
-    event_argument = $2
-    form = page.form_with(id: "aspnetForm")
-    form["__EVENTTARGET"] = event_target
-    form["__EVENTARGUMENT"] = event_argument
-    form.submit
-  elsif js =~ /return false;__doPostBack\('(.*)','(.*)'\)/
-    nil
-  else
-    # TODO Just follow the link likes it's a normal link
-    raise
-  end
-end
-
-url = "http://pdonline.logan.qld.gov.au/MasterViewUI/Modules/ApplicationMaster/default.aspx?page=found&1=thismonth&4a=&6=F"
-comment_url = "mailto:council@logan.qld.gov.au"
+url = "http://pdonline.brisbane.qld.gov.au/MasterView/modules/applicationmaster/default.aspx?page=found&1=thismonth&6=F"
 
 agent = Mechanize.new
 
@@ -53,20 +35,10 @@ page = agent.get(url)
 
 # This is weird. There are two forms with the Agree / Disagree buttons. One of them
 # works the other one doesn't. Go figure.
-form = page.forms[1]
-button = form.button_with(value: "Agree")
+form = page.forms.first
+button = form.button_with(value: "I agree")
 raise "Can't find agree button" if button.nil?
 page = form.submit(button)
+page = agent.get(url)
 
-current_page_no = 1
-next_page_link = true
-
-while next_page_link
-  puts "Scraping page #{current_page_no}..."
-  scrape_page(page, comment_url)
-
-  current_page_no += 1
-  next_page_link = page.at(".rgPageNext")
-  page = click(page, next_page_link)
-  next_page_link = nil if page.nil?
-end
+scrape_page(page)
