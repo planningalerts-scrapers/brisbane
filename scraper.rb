3aa9032a636b063dcff072b096d421c34078cf90
Matthew Landauer <matthew@openaustralia.org>
2014-04-09 10:57:14 +1000

    Copy and paste from logan

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,72 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'mechanize'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# Scraping from Masterview 2.0
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+def scrape_page(page, comment_url)
+  page.at("table.rgMasterTable").search("tr.rgRow,tr.rgAltRow").each do |tr|
+    tds = tr.search('td').map{|t| t.inner_html.gsub("\r\n", "").strip}
+    day, month, year = tds[2].split("/").map{|s| s.to_i}
+    record = {
+      "info_url" => (page.uri + tr.search('td').at('a')["href"]).to_s,
+      "council_reference" => tds[1],
+      "date_received" => Date.new(year, month, day).to_s,
+      "description" => tds[3].gsub("&amp;", "&").split("<br>")[1].squeeze(" ").strip,
+      "address" => tds[3].gsub("&amp;", "&").split("<br>")[0].gsub("\r", " ").gsub("<strong>","").gsub("</strong>","").squeeze(" ").strip,
+      "date_scraped" => Date.today.to_s,
+      "comment_url" => comment_url
+    }
+    #p record
+    if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
+      ScraperWiki.save_sqlite(['council_reference'], record)
+    else
+      puts "Skipping already saved record " + record['council_reference']
+    end
+  end
+end
+
+# Implement a click on a link that understands stupid asp.net doPostBack
+def click(page, doc)
+  js = doc["href"] || doc["onclick"]
+  if js =~ /javascript:__doPostBack\('(.*)','(.*)'\)/
+    event_target = $1
+    event_argument = $2
+    form = page.form_with(id: "aspnetForm")
+    form["__EVENTTARGET"] = event_target
+    form["__EVENTARGUMENT"] = event_argument
+    form.submit
+  elsif js =~ /return false;__doPostBack\('(.*)','(.*)'\)/
+    nil
+  else
+    # TODO Just follow the link likes it's a normal link
+    raise
+  end
+end
+
+url = "http://pdonline.logan.qld.gov.au/MasterViewUI/Modules/ApplicationMaster/default.aspx?page=found&1=thismonth&4a=&6=F"
+comment_url = "mailto:council@logan.qld.gov.au"
+
+agent = Mechanize.new
+
+# Read in a page
+page = agent.get(url)
+
+# This is weird. There are two forms with the Agree / Disagree buttons. One of them
+# works the other one doesn't. Go figure.
+form = page.forms[1]
+button = form.button_with(value: "Agree")
+raise "Can't find agree button" if button.nil?
+page = form.submit(button)
+
+current_page_no = 1
+next_page_link = true
+
+while next_page_link
+  puts "Scraping page #{current_page_no}..."
+  scrape_page(page, comment_url)
+
+  current_page_no += 1
+  next_page_link = page.at(".rgPageNext")
+  page = click(page, next_page_link)
+  next_page_link = nil if page.nil?
+end
