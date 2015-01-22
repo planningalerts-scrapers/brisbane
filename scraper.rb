de906ba022841f68a695426b1d2eac8c0da21892
Luke Bacon <luke@equivalentideas.com>
2015-01-22 21:27:28 +1100

    remove 'put record' used to show output in dev

--- scraper.rb
+++ scraper.rb
@@ -19,7 +19,6 @@ def scrape_page(page)
     #p record
     if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
       ScraperWiki.save_sqlite(['council_reference'], record)
-      puts record
     else
       puts "Skipping already saved record " + record['council_reference']
     end
