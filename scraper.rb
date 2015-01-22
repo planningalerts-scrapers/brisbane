91051863115ed1c4f01b4d7547fe911e68860ae6
Luke Bacon <luke@equivalentideas.com>
2015-01-22 21:26:40 +1100

    cycle through the pages I took this method from https://github.com/planningalerts-scrapers/north_sydney/blob/master/scraper.rb#L51

--- scraper.rb
+++ scraper.rb
@@ -26,6 +26,27 @@ def scrape_page(page)
   end
 end
 
+def scrape_and_follow_next_link(page)
+  scrape_page(page)
+  nextButton = page.at('.rgPageNext')
+  puts "No further pages" if nextButton.nil?
+  unless nextButton.nil? || nextButton['onclick'] =~ /return false/
+    form = page.forms.first
+
+    # The joy of dealing with ASP.NET
+    form['__EVENTTARGET'] = nextButton['name']
+    form['__EVENTARGUMENT'] = ''
+    # It doesn't seem to work without these stupid values being set.
+    # Would be good to figure out where precisely in the javascript these values are coming from.
+    form['ctl00%24RadScriptManager1']=
+    'ctl00%24cphContent%24ctl00%24ctl00%24cphContent%24ctl00%24Radajaxpanel2Panel%7Cctl00%24cphContent%24ctl00%24ctl00%24RadGrid1%24ctl00%24ctl03%24ctl01%24ctl10'
+    form['ctl00_RadScriptManager1_HiddenField']=
+    '%3B%3BSystem.Web.Extensions%2C%20Version%3D3.5.0.0%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D31bf3856ad364e35%3Aen-US%3A0d787d5c-3903-4814-ad72-296cea810318%3Aea597d4b%3Ab25378d2%3BTelerik.Web.UI%2C%20Version%3D2009.1.527.35%2C%20Culture%3Dneutral%2C%20PublicKeyToken%3D121fae78165ba3d4%3Aen-US%3A1e3fef00-f492-4ed8-96ce-6371bc241e1c%3A16e4e7cd%3Af7645509%3A24ee1bba%3Ae330518b%3A1e771326%3Ac8618e41%3A4cacbc31%3A8e6f0d33%3Aed16cbdc%3A58366029%3Aaa288e2d'
+    page = form.submit(form.button_with(:name => nextButton['name']))
+    scrape_and_follow_next_link(page)
+  end
+end
+
 url = "https://pdonline.brisbane.qld.gov.au/MasterViewUI/Modules/ApplicationMaster/default.aspx?page=found&1=thismonth&6=F"
 
 agent = Mechanize.new
@@ -41,4 +62,4 @@ raise "Can't find agree button" if button.nil?
 page = form.submit(button)
 page = agent.get(url)
 
-scrape_page(page)
+scrape_and_follow_next_link(page)
